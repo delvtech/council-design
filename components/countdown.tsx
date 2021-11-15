1ae7a03d1bfd26586c0bbd53548b8fd79f7ea107
Matthew Brown <mattfdbrown@gmail.com>
2021-11-15 16:08:42 -0700

    update tailwindcss-classnames

--- components/countdown.tsx
+++ components/countdown.tsx
@@ -1,79 +1,73 @@
 import React, { useEffect, useState } from "react";
 
+import { tw } from "src/elf-tailwindcss-classnames";
+
 const calculateSeconds = (time: {
-	hours?: number;
-	minutes?: number;
-	seconds?: number;
+  hours?: number;
+  minutes?: number;
+  seconds?: number;
 }) => {
-	const { hours, minutes, seconds } = time;
-	if (!(hours || minutes || seconds)) {
-		throw new Error(
-			"You must provide at least one value of hours, minutes or seconds"
-		);
-	}
-	return (hours || 0) * 3600 + (minutes || 0) * 60 + (seconds || 0);
+  const { hours, minutes, seconds } = time;
+  if (!(hours || minutes || seconds)) {
+    throw new Error(
+      "You must provide at least one value of hours, minutes or seconds"
+    );
+  }
+  return (hours || 0) * 3600 + (minutes || 0) * 60 + (seconds || 0);
 };
 
 const formatTime = (seconds: number, format: string) => {
-	const hours = Math.floor(seconds / 3600);
-	const minutes = Math.floor((seconds % 3600) / 60);
-	const secondsLeft = seconds % 60;
-	const formattedTime = format
-		.replace("hh", hours.toString().padStart(2, "0"))
-		.replace("mm", minutes.toString().padStart(2, "0"))
-		.replace("ss", secondsLeft.toString().padStart(2, "0"));
-	return formattedTime;
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const secondsLeft = seconds % 60;
+  const formattedTime = format
+    .replace("hh", hours.toString().padStart(2, "0"))
+    .replace("mm", minutes.toString().padStart(2, "0"))
+    .replace("ss", secondsLeft.toString().padStart(2, "0"));
+  return formattedTime;
 };
 
 export interface CountdownProps extends React.HTMLAttributes<HTMLSpanElement> {
-	hours?: number;
-	minutes?: number;
-	seconds?: number;
-	timeFormat?: string;
-	onComplete?: () => void;
+  hours?: number;
+  minutes?: number;
+  seconds?: number;
+  timeFormat?: string;
+  onComplete?: () => void;
 }
 
 let intervalId: NodeJS.Timeout;
 
 export default function Countdown({
-	className,
-	hours,
-	minutes,
-	seconds,
-	timeFormat,
-	onComplete,
-	...props
+  className,
+  hours,
+  minutes,
+  seconds,
+  timeFormat,
+  onComplete,
+  ...props
 }: CountdownProps) {
-	const [secondsLeft, setsecondsLeft] = useState(
-		calculateSeconds({ hours, minutes, seconds })
-	);
-	const format = timeFormat || "hhH mmM ssS";
+  const [secondsLeft, setsecondsLeft] = useState(
+    calculateSeconds({ hours, minutes, seconds })
+  );
+  const format = timeFormat || "hhH mmM ssS";
 
-	useEffect(() => {
-		intervalId = setInterval(() => {
-			setsecondsLeft((secondsLeft) => secondsLeft - 1);
-		}, 1000);
-		return () => intervalId && clearInterval(intervalId);
-	}, []);
+  useEffect(() => {
+    intervalId = setInterval(() => {
+      setsecondsLeft((secondsLeft) => secondsLeft - 1);
+    }, 1000);
+    return () => intervalId && clearInterval(intervalId);
+  }, []);
 
-	useEffect(() => {
-		if (secondsLeft <= 0) {
-			intervalId && clearInterval(intervalId);
-			onComplete && onComplete();
-		}
-	}, [secondsLeft]);
+  useEffect(() => {
+    if (secondsLeft <= 0) {
+      intervalId && clearInterval(intervalId);
+      onComplete && onComplete();
+    }
+  }, [secondsLeft]);
 
-	return (
-		<span
-			className={`
-			text-principalRoyalBlue
-			text-base
-
-			${className || ""}
-			`}
-			{...props}
-		>
-			{formatTime(secondsLeft, format)}
-		</span>
-	);
+  return (
+    <span className={tw("text-principalRoyalBlue", "text-base")} {...props}>
+      {formatTime(secondsLeft, format)}
+    </span>
+  );
 }
