53ca868cc7cf7268e9c0bd3191c05943541ea713
Danny Delott <dannydelott@gmail.com>
2021-11-19 18:28:23 -0800

    Configure storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,16 @@
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
 module.exports = {
+  webpackFinal: async (config) => {
+    config.resolve.plugins.push(new TsconfigPathsPlugin({}));
+    return config;
+  },
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+
+    // TODO: delete this once all stories are moved into feature folders
+    // "../stories/**/*.stories.mdx",
+    // "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
